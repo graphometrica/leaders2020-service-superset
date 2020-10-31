cc811ad627716fd64d12650e02a3815c91dd8130
SemyonSinchenko <semyonsinchenko@yahoo.com>
2020-10-31 17:17:52 +0300

    fix99

--- superset_config.py
+++ superset_config.py
@@ -43,7 +43,7 @@ def get_env_variable(var_name, default=None):
             )
             raise EnvironmentError(error_msg)
 
-
+AUTH_TYPE = 1
 #DATABASE_DIALECT = get_env_variable("DATABASE_DIALECT")
 #DATABASE_USER = get_env_variable("DATABASE_USER")
 #DATABASE_PASSWORD = get_env_variable("DATABASE_PASSWORD")
@@ -62,39 +62,39 @@ def get_env_variable(var_name, default=None):
 #)
 SQLALCHEMY_DATABASE_URI = "postgresql://graph:graph@23.251.145.120:5432/superset"
 
-REDIS_HOST = get_env_variable("REDIS_HOST")
-REDIS_PORT = get_env_variable("REDIS_PORT")
-REDIS_CELERY_DB = get_env_variable("REDIS_CELERY_DB", 0)
-REDIS_RESULTS_DB = get_env_variable("REDIS_CELERY_DB", 1)
-
-
-RESULTS_BACKEND = FileSystemCache("/app/superset_home/sqllab")
-
-
-class CeleryConfig(object):
-    BROKER_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_CELERY_DB}"
-    CELERY_IMPORTS = ("superset.sql_lab",)
-    CELERY_RESULT_BACKEND = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_RESULTS_DB}"
-    CELERY_ANNOTATIONS = {"tasks.add": {"rate_limit": "10/s"}}
-    CELERY_TASK_PROTOCOL = 1
-
-
-CELERY_CONFIG = CeleryConfig
-SQLLAB_CTAS_NO_LIMIT = True
-
+# REDIS_HOST = get_env_variable("REDIS_HOST")
+# REDIS_PORT = get_env_variable("REDIS_PORT")
+# REDIS_CELERY_DB = get_env_variable("REDIS_CELERY_DB", 0)
+# REDIS_RESULTS_DB = get_env_variable("REDIS_CELERY_DB", 1)
 #
-# Optionally import superset_config_docker.py (which will have been included on
-# the PYTHONPATH) in order to allow for local settings to be overridden
 #
-try:
-    import superset_config_docker
-    from superset_config_docker import *  # noqa
-
-    logger.info(
-        f"Loaded your Docker configuration at " f"[{superset_config_docker.__file__}]"
-    )
-except ImportError:
-    logger.info("Using default Docker config...")
+# RESULTS_BACKEND = FileSystemCache("/app/superset_home/sqllab")
+#
+#
+# class CeleryConfig(object):
+#     BROKER_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_CELERY_DB}"
+#     CELERY_IMPORTS = ("superset.sql_lab",)
+#     CELERY_RESULT_BACKEND = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_RESULTS_DB}"
+#     CELERY_ANNOTATIONS = {"tasks.add": {"rate_limit": "10/s"}}
+#     CELERY_TASK_PROTOCOL = 1
+#
+#
+# CELERY_CONFIG = CeleryConfig
+# SQLLAB_CTAS_NO_LIMIT = True
+#
+# #
+# # Optionally import superset_config_docker.py (which will have been included on
+# # the PYTHONPATH) in order to allow for local settings to be overridden
+# #
+# try:
+#     import superset_config_docker
+#     from superset_config_docker import *  # noqa
+#
+#     logger.info(
+#         f"Loaded your Docker configuration at " f"[{superset_config_docker.__file__}]"
+#     )
+# except ImportError:
+#     logger.info("Using default Docker config...")
 
 BABEL_DEFAULT_LOCALE = "en"
 
